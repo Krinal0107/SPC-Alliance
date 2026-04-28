0c10cd3f3d92b39137e2d3046646fd43714e0dbb
Krinal <krinaltagadiya999@gmail.com>
2026-04-28 09:44:07 +0530

    commit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@base-org/account': false,
+      '@coinbase/wallet-sdk': false,
+      '@metamask/connect-evm': false,
+      '@safe-global/safe-apps-sdk': false,
+      '@safe-global/safe-apps-provider': false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
